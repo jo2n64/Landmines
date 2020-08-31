54b580708ae8674cea7f00c135dd549bd54059c7
jo2n64 <valio31032@gmail.com>
2020-08-31 15:53:56 +0200

    added mouse controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float sensitivity;
+
+    [SerializeField] private GameObject player;
+
+    private float yAxisClamp; // just so it reaches a limit at top and bottom
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Look();
+    }
+
+
+    void Look()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity * Time.deltaTime;
+
+        yAxisClamp += mouseY;
+
+
+        if (yAxisClamp >= 90f)
+        {
+            yAxisClamp = 90f;
+            mouseY = 0f;
+        }
+
+        else if (yAxisClamp <= -90f)
+        {
+            yAxisClamp = -90f;
+            mouseY = 0f;
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+
+        player.transform.Rotate(Vector3.up * mouseX);
+
+
+    }
+}
---
+++ Assets/Scripts/PersonMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonMovement : MonoBehaviour
+{
+    float speed = 5f;
+    private CharacterController cc;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = transform.forward * vertical * speed * Time.deltaTime;
+        Vector3 right = transform.right * horizontal * speed * Time.deltaTime;
+
+        cc.Move(forward + right);
+    }
+}
